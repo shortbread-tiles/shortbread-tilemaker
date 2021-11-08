dd8e57a91dee5fe6b7ecd2d29dd7c3f4ccf3e1d8
Michael Reichert <michael.reichert@geofabrik.de>
2021-11-08 14:14:55 +0100

    Write features to streets_polygons_labels if they have a name only

--- process.lua
+++ process.lua
@@ -641,10 +641,12 @@ function process_street_polygons(way)
 			way:Attribute("service", service)
 		end
 		setZOrder(way, rail, false)
-		way:LayerAsCentroid("streets_polygons_labels")
-		setNameAttributes(way)
-		way:Attribute("kind", kind)
-		way:MinZoom(mz)
+		if name ~= "" then
+			way:LayerAsCentroid("streets_polygons_labels")
+			setNameAttributes(way)
+			way:Attribute("kind", kind)
+			way:MinZoom(mz)
+		end
 	end
 end
 
