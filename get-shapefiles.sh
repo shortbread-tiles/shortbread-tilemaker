47e3ca446b313f934ff9cbb5814d44053cd51cca
Michael Reichert <michael.reichert@geofabrik.de>
2022-02-18 15:51:38 +0100

    Improve get-shapefiles.sh script

--- get-shapefiles.sh
+++ get-shapefiles.sh
@@ -1,14 +1,27 @@
 #! /bin/bash
 
+set -euo pipefail
+
+function download_and_extract {
+    URL=$1
+    FILE=$2
+    START_DIR=$(pwd)
+    cd data
+    curl -z "$FILE" -LO "$URL"
+    unzip -u "$FILE"
+    cd "$START_DIR"
+}
+
+function transform_shp {
+    FILE=$1
+    OUT_FILE=$2
+    ogr2ogr -f "ESRI Shapefile" "$OUT_FILE" "$FILE" -t_srs EPSG:4326 -lco ENCODING=utf8
+}
+
 cd "$(dirname "$0")" || exit
 
 mkdir -p data
-cd data || exit
-if [ ! -s water-polygons-split-4326.zip ] ; then
-	curl -LO "https://osmdata.openstreetmap.de/download/water-polygons-split-4326.zip"
-fi
-
-if [ water-polygons-split-4326.zip -nt water-polygons-split-4326/water_polygons.shp ] ; then
-	unzip -u water-polygons-split-4326.zip
-	touch water-polygons-split-4326/*
-fi
+download_and_extract "https://osmdata.openstreetmap.de/download/water-polygons-split-4326.zip" "water-polygons-split-4326.zip"
+download_and_extract "https://osmdata.openstreetmap.de/download/simplified-water-polygons-split-3857.zip" "simplified-water-polygons-split-3857.zip"
+mkdir -p data/simplified-water-polygons-split-4326
+transform_shp data/simplified-water-polygons-split-3857/simplified_water_polygons.shp data/simplified-water-polygons-split-4326/simplified_water_polygons.shp
