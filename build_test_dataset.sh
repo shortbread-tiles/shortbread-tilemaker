20e4affb4c2c5b8d4610f3a81502bc5ee3c31db9
Michael Reichert <michael.reichert@geofabrik.de>
2022-09-02 14:52:07 +0200

    Add script to build test datasets

---
+++ build_test_dataset.sh
@@ -0,0 +1,42 @@
+#! /usr/bin/env bash
+
+# Build test dataset
+
+set -euo pipefail
+
+TILEMAKER=${TILEMAKER:-tilemaker}
+
+if [ "$#" -lt 4 ]; then
+    echo "ERROR: Wrong Usage"
+    echo "Usage: $0 BBOX SMALL_OSM_PBF LARGE_OSM_PBF OUT_DIRECTORY [TILEMAKER_EXTRA_ARGS]"
+    exit 1
+fi
+
+BBOX=$1
+shift
+SMALL_OSM_PBF=$1
+shift
+LARGE_OSM_PBF=$1
+shift
+OUT_DIR=$1
+shift
+
+if [ ! -d "$OUT_DIR" ]; then
+    echo "ERROR: Output directory $OUT_DIR does not exist."
+    exit 1
+fi
+
+EXTRA_ARGS=$@
+echo "Make small region first."
+mkdir -p $OUT_DIR/small
+"$TILEMAKER" $EXTRA_ARGS --input $SMALL_OSM_PBF --output $OUT_DIR/small --config config.json --process process.lua
+
+echo "Make large region first."
+mkdir -p $OUT_DIR/large
+jq '.settings.maxzoom |= 7' config.json > config-lowzoom.json
+"$TILEMAKER" $EXTRA_ARGS --bbox=$BBOX --input $LARGE_OSM_PBF --output $OUT_DIR/small --config config-lowzoom.json --process process.lua
+
+echo "Merging"
+rsync -a "$OUT_DIR/large/" "$OUT_DIR/small/"
+
+echo "Result written to $OUT_DIR/small/"
